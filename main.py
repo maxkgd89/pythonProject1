46e50e7a335eb256f928fff645bb62fe7c56a38f
maxkgd89 <37918183+maxkgd89@users.noreply.github.com>
2024-03-11 00:57:11 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,99 @@
+import time
+import random
+import math
+import sys
+
+people = [('Lisbon', 'LIS'),
+          ('Madrid', 'MAD'),
+          ('Paris', 'CDG'),
+          ('Dublin', 'DUB'),
+          ('Brussels', 'BRU'),
+          ('London', 'LHR')]
+
+flights = {}
+for line in open('flights.txt'):
+    origin, destiny, departure, arrival, price = line.split(',')
+    flights.setdefault((origin, destiny), [])
+    flights[(origin, destiny)].append((departure, arrival, int(price)))
+
+# for key, value in flights.items():
+#  print(key, value)
+
+def print_schedule(schedule):
+    flight_id = -1
+    total_price = 0
+    for i in range(len(schedule) // 2):
+        name = people[i][0]
+        origin = people[i][1]
+        flight_id += 1
+        going = flights[(origin, destiny)][schedule[flight_id]]
+        total_price += going[2]
+        flight_id += 1
+        returning = flights[(destiny, origin)][schedule[flight_id]]
+        total_price += returning[2]
+        print('%10s%10s %5s-%5s U$%3s %5s-%5s U$%3s' % (name, origin, going[0],
+                                                        going[1], going[2],
+                                                        returning[0], returning[1], returning[2]))
+    print('Total price: ', total_price)
+def get_minutes(hour):
+    t = time.strptime(hour, '%H:%M')
+    minutes = t[3] * 60 + t[4]
+    return minutes
+def fitness_function(solution):
+    total_price = 0
+    last_arrival = 0
+    first_departure = 1439
+
+    flight_id = -1
+    for i in range(len(solution) // 2):
+        origin = people[i][1]
+        flight_id += 1
+        going = flights[(origin, destiny)][solution[flight_id]]
+        flight_id += 1
+        returning = flights[(destiny, origin)][solution[flight_id]]
+
+        total_price += going[2]
+        total_price += returning[2]
+
+        if last_arrival < get_minutes(going[1]):
+            last_arrival = get_minutes(going[1])
+        if first_departure > get_minutes(returning[0]):
+            first_departure = get_minutes(returning[0])
+
+    total_wait = 0
+    flight_id = -1
+    for i in range(len(solution) // 2):
+        origin = people[i][1]
+        flight_id += 1
+        going = flights[(origin, destiny)][solution[flight_id]]
+        flight_id += 1
+        returning = flights[(destiny, origin)][solution[flight_id]]
+
+        total_wait += last_arrival - get_minutes(going[1])
+        total_wait += get_minutes(returning[0]) - first_departure
+
+    # 3PM - 10AM
+    # 11AM - 3PM
+    if last_arrival > first_departure:
+        total_price += 50
+
+    return total_price + total_wait
+
+domain = [(0,9)] * (len(people) * 2)
+[random.randint(0,9) for i in range(len(domain))]
+
+def random_search(domain, fitness_function):
+  best_cost = sys.maxsize
+  for i in range(1000):
+    solution = [random.randint(domain[i][0],domain[i][1]) for i in range(len(domain))]
+    cost = fitness_function(solution)
+    if cost < best_cost:
+      best_cost = cost
+      best_solution = solution
+  return best_solution
+
+random_solution = random_search(domain, fitness_function)
+
+fitness_function(random_solution)
+
+print_schedule(random_solution)
